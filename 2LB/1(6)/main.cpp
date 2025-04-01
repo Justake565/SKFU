62d361f994abe87397d6c1bef582e0598a1105e9
Justake565 <romaolesya@bk.ru>
2025-04-01 13:05:48 +0300

    Update main.cpp

--- 2LB/1(6)/main.cpp
+++ 2LB/1(6)/main.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include "Time.h"
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int minutes, seconds;
+    cout << "Введите время: " << endl;
+    cin >> minutes >> seconds;
+    TimeConverter* problem = new TimeConverter(minutes, seconds); //Создание объекта класса
+    problem->console();
+    delete problem;
+    return 0;
+};
 
