d08ab5b9fd732510f720709b59a8c46a7092e576
Justake565 <romaolesya@bk.ru>
2025-04-01 13:06:27 +0300

    Update Time.h

--- 2LB/1(6)/Time.h
+++ 2LB/1(6)/Time.h
@@ -1,1 +1,13 @@
+#pragma once
+class TimeConverter { // Класс, объявляется с двумя приватными полями
+   
+public:
+        TimeConverter(int minutes, int seconds); // Конструктор
+        void console();
+        double half_TimeConverter(int minutes, int seconds);
+
+    private:
+        int minutes;
+        int seconds;
+    };
 
