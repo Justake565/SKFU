27cc14e9d0b047c0a9075b0dab130e91799c4d84
Justake565 <romaolesya@bk.ru>
2025-04-01 13:09:10 +0300

    Update main.cpp

--- 2LB/2(6)/main.cpp
+++ 2LB/2(6)/main.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include "Time.h"
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    Time time1(13, 25, 30);
+
+    // Выводим информацию об объекте
+    cout << "Исходное время: " << time1.info() << endl;
+
+    // Определяем количество минут до полуночи
+    cout << "Минут до полуночи: " << time1.minutesToMidnight() << endl;
+
+    // Увеличиваем время на 100 минут
+    time1.add100Minutes();
+
+    cout << "Время после добавления 100 минут: " << time1.info() << endl;
+
+    return 0;
+}
 
