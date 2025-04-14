8835af2e27bb01b53e7c1f40e30a06dc7d9d3a66
Justake565 <romaolesya@bk.ru>
2025-04-14 21:06:24 +0300

    Update 3(6).cpp

--- 3LB/3(6).cpp
+++ 3LB/3(6).cpp
@@ -6,11 +6,9 @@ int main() {
     setlocale(LC_ALL, "Russian");
     string text;
 
-    // Ввод текста от пользователя
     cout << "Введите строку: ";
     getline(cin, text);
 
-    // Проверяем, достаточно ли символов в строке
     if (text.length() < 7) {
         cout << "Строка должна содержать как минимум 7 символов." << endl;
         return 1;
@@ -25,7 +23,6 @@ int main() {
     // Заменяем последние 7 символов в исходной строке на отсортированные
     text.replace(text.length() - 7, 7, lastSeven);
 
-    // Выводим результат
     cout << "Результат: " << text << endl;
 
     return 0;
