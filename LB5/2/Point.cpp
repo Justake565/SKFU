863c8484c64243eee23f45aa85794c2d40cac075
Justake565 <romaolesya@bk.ru>
2025-05-18 18:25:32 +0300

    Update Point.cpp

--- LB5/2/Point.cpp
+++ LB5/2/Point.cpp
@@ -5,21 +5,3 @@ using namespace std;
 void Point::Show() const {
 	cout << " (" << x << y << ").";
 }
-Point.h:
-#pragma once
-#ifndef POINT_H
-#define POINT_H
-
-class Point {
-public:
-	
-	Point(double _õ = 0, double _ó = 0) : x(_õ), y(_ó) {}
-
-	
-	void Show() const;
-
-public:
-	double x, y;
-};
-
-#endif
