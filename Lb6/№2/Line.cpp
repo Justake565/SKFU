7bafac06fbec61fb0fba22ec0ac55ce64ccc8d60
Justake565 <romaolesya@bk.ru>
2025-05-24 22:15:21 +0300

    Create Line.cpp

---
+++ Lb6/№2/Line.cpp
@@ -0,0 +1,12 @@
+#include "Line.h"
+#include <iostream>
+
+using namespace std;
+
+void Line::Set_Coeff() {
+	cout << "Введите a: "; cin >> a;
+	cout << "Введите b: "; cin >> b;
+}
+
+
+Line f_line;
